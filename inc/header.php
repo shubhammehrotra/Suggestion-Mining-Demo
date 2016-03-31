a8f8333306a719bab3891bea1640bf5f6aef1b4c
shubhammehrotra94 <shubhammehrotra94@gmail.com>
2016-03-31 10:08:16 +0100

    test

--- inc/header.php
+++ inc/header.php
@@ -27,7 +27,7 @@
 
     <div id="wrapper">
 
-        <nav class="navbar navbar-fixed-top" role="navigation">
+        <nav class = "navbar navbar-inverse navbar-fixed-top" role="navigation">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
