6ffaf583d72071beb039269947c1a95618966266
shubhammehrotra94 <shubhammehrotra94@gmail.com>
2016-03-31 10:44:59 +0100

    merged code

--- about.php
+++ about.php
@@ -37,7 +37,7 @@
             </div>
         </div>
     </div>
-
+ 
     <div class="row">
         <div class="col-md-12 ">
             <div class="panel panel-default">
---
+++ inc/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+ 
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="images/random.ico">
+ 
+        <title>Suggestion Extraction</title>
+ 
+        <!-- Bootstrap core CSS -->
+        <link href="css/style.css" rel="stylesheet">
+ 
+        <!-- Custom styles for this template -->
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+ 
+</head>
+ 
+<body>
+        <header>
+            <div class="container">
+                <div class="alighleft">
+               <a href="./"> <span class="logo"><span class="blue">MiSO</span></span></a>
+                Mine Suggestions from Opinions
+                </div>   
+                 
+                <ul class="alighright">
+                    <li>
+                        <a href="./"> <span class="glyphicon glyphicon-home"></span>  Home </a>
+                    </li>
+                    <li>   
+                        <a href="about.php"></i> <span class="glyphicon glyphicon-info-sign"> </span> About</a>
+                    </li>
+                    <li>
+                        <a href="restapi.php"> <span class="glyphicon glyphicon-cog"> </span> Rest API</a>
+                    </li>
+                    <li>
+                            <span class="glyphicon glyphicon-briefcase"> </span> Use Cases 
+                            <ul> 
+                     <!--   <ul class="dropdown-menu" id> -->
+                            <li><a href="reviews.php">Online Reviews</a></li>
+                            <li><a href="debates.php">Debates</a></li>
+                            <li><a href="twitter.php">Twitter Data</a></li>
+                        </ul>         
+                    </li>
+                </ul>
+                <div class="clear"></div>
+          </header>
+        <br>
+        <br>
+    <!-- /.row -->
