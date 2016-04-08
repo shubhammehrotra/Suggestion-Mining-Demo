26c0ac0afeae935042de21c1539ff7860f924b7c
shubhammehrotra94 <shubhammehrotra94@gmail.com>
2016-04-08 16:31:49 +0100

    hotels

--- reviews.php
+++ reviews.php
@@ -23,23 +23,43 @@
         .node--root,
         .node--leaf {
             pointer-events: none;
-        } 
+        }
     </style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <div class="col-md-3">
-        <ul class="nav nav-pills nav-stacked admin-menu " id="list1">
-            <li><a><b>Product Categories</a></b>
-            </li>
-            <li><a href="#" id="Pr1">Product 1</a></li>
-            <li><a href="#" id="P2">Product 2</a></li>
-            <li><a href="#" id="P3">Product 3</a></li>
-            <li><a href="#" id="P4">Product 4</a></li>
-            <li><a href="#" id="P5">Product 5</a></li>
-            <li><a href="#" id="Pr6">Product 6</a></li>
-        </ul>
+        <div class="container">
+            <ul class="nav nav-tabs">
+                <li class="active"><a data-toggle="tab" href="#product">Product Reviews</a></li>
+                <li><a data-toggle="tab" href="#hotel">Hotel Reviews</a></li>
+            </ul>
+
+            <div class="tab-content">
+                <div id="product" class="tab-pane fade in active">
+                    <ul class="nav nav-pills nav-stacked admin-menu " id="list1">
+                        <li><a href="#" id="Pr1">Product 1</a></li>
+                        <li><a href="#" id="P2">Product 2</a></li>
+                        <li><a href="#" id="P3">Product 3</a></li>
+                        <li><a href="#" id="P4">Product 4</a></li>
+                        <li><a href="#" id="P5">Product 5</a></li>
+                        <li><a href="#" id="Pr6">Product 6</a></li>
+                    </ul>
+                </div>
+                <div id="hotel" class="tab-pane fade">
+                    <ul class="nav nav-pills nav-stacked admin-menu " id="list2">
+                        <li><a href="#" id="Pr1">Hotel 1</a></li>
+                        <li><a href="#" id="P2">Hotel 2</a></li>
+                        <li><a href="#" id="P3">Hotel 3</a></li>
+                        <li><a href="#" id="P4">Hotel 4</a></li>
+                        <li><a href="#" id="P5">Hotel 5</a></li>
+                        <li><a href="#" id="Pr6">Hotel 6</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
     </div>
     <div class="col-md-9 well admin-content" id="outer1" hidden>
         <pre id="p1" hidden></pre>
@@ -53,6 +73,128 @@
         //s = d3.selectAll('svg')
 
         $('#list1').on('click', 'li', function () {
+            $('#outer1').show();
+            var s = d3.select("#p6").selectAll('#graphic').remove();
+            var clicked = $(this).text();
+            check = 1;
+            //alert(clicked);
+            $('#p1').show();
+            $('#p6').show();
+            $('#pr6').hide();
+            document.getElementById("p6").innerHTML = "";
+            $('#p1').text("Suggestions to be fetched from the api for:  " + clicked);
+            var margin = 0,
+                diameter = 400;
+
+            var color = d3.scale.linear()
+                .domain([-1, 5])
+                .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
+                .interpolate(d3.interpolateHcl);
+
+            var pack = d3.layout.pack()
+                .padding(2)
+                .size([diameter - margin, diameter - margin])
+                .value(function (d) {
+                    return d.size;
+                })
+
+            var svg = d3.select("#p6").append("svg")
+                .attr("width", diameter)
+                .attr("height", diameter)
+                .append("g")
+                .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")")
+                .attr("id", "graphic");
+            d3.json("flare.json", function (error, root) {
+                if (error) throw error;
+
+                var focus = root,
+                    nodes = pack.nodes(root),
+                    view;
+
+                var circle = svg.selectAll("circle")
+                    .data(nodes)
+                    .enter().append("circle")
+                    .attr("class", function (d) {
+                        return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root";
+                    })
+                    .style("fill", function (d) {
+                        return d.children ? color(4.3) : null;
+                    })
+                    .on("click", function (d) {
+                        $('#p1').hide();
+                        $('#pr6').show();
+                        $('#pr6').text("Suggestions to be shown for the topic : " + d.name);
+                        if (focus !== d) zoom(d), d3.event.stopPropagation();
+                    });
+
+                var text = svg.selectAll("text")
+                    .data(nodes)
+                    .enter().append("text")
+                    .attr("class", "label")
+                    .style("fill-opacity", function (d) {
+                        return d.parent === root ? 1 : 0;
+                    })
+                    .style("display", function (d) {
+                        return d.parent === root ? "inline" : "none";
+                    })
+                    .text(function (d) {
+                        return d.name;
+                    });
+
+                var node = svg.selectAll("circle,text");
+
+                d3.select("body")
+                    .on("click", function () {
+                        zoom(root);
+                    });
+
+                zoomTo([root.x, root.y, root.r * 2 /*+ margin*/ ]);
+
+                function zoom(d) {
+                    var focus0 = focus;
+                    focus = d;
+
+                    var transition = d3.transition()
+                        .duration(d3.event.altKey ? 7500 : 750)
+                        .tween("zoom", function (d) {
+                            var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 /*+ margin*/ ]);
+                            return function (t) {
+                                zoomTo(i(t));
+                            };
+                        });
+
+                    transition.selectAll("text")
+                        .filter(function (d) {
+                            return d.parent === focus || this.style.display === "inline";
+                        })
+                        .style("fill-opacity", function (d) {
+                            return d.parent === focus ? 1 : 0;
+                        })
+                        .each("start", function (d) {
+                            if (d.parent === focus) this.style.display = "inline";
+                        })
+                        .each("end", function (d) {
+                            if (d.parent !== focus) this.style.display = "none";
+                        });
+                }
+
+                function zoomTo(v) {
+                    var k = diameter / v[2];
+                    view = v;
+                    node.attr("transform", function (d) {
+                        return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")";
+                    });
+                    circle.attr("r", function (d) {
+                        return d.r * k;
+                    });
+                }
+            });
+            /*var active = graphic.active ? false : true,
+                newOpacity = active ? 0 : 1;
+            d3.select("#graphic").style("opacity", newOpacity);
+            graphic.active = active;*/
+            //$('#p4').hide();
+        });$('#list2').on('click', 'li', function () {
             $('#outer1').show();
             var s = d3.select("#p6").selectAll('#graphic').remove();
             var clicked = $(this).text();
